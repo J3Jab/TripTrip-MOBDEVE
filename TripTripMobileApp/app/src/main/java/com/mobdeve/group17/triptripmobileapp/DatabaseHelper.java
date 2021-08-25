c1b629758af2cc461d57d555914d4566925d2c46
j3jabanes <70612391+j3jabanes@users.noreply.github.com>
2021-08-26 01:59:05 +0800

    Updated DBhelper with Trips table

--- TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/DatabaseHelper.java
+++ TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/DatabaseHelper.java
@@ -15,7 +15,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // Database Version
     private static final int DATABASE_VERSION = 1;
     // Database Name
-    private static final String DATABASE_NAME = "Login.db";
+    private static final String DATABASE_NAME = "TripTrip.db";
 
     // User table name
     private static final String TABLE_USER = "user";
@@ -23,12 +23,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_USER_BDAY = "user_bday";
     private static final String COLUMN_USER_EMAIL = "user_email";
     private static final String COLUMN_USER_PASSWORD = "user_password";
+
+    // Trip table name
+    private static final String TABLE_TRIP = "trip";
+    private static final String COLUMN_TRIP_ID = "trip_id";
+    private static final String COLUMN_TRIP_TITLE = "trip_title";
+    private static final String COLUMN_TRIP_STARTDATE = "trip_startdate";
+    private static final String COLUMN_TRIP_ENDDATE = "trip_enddate";
+    private static final String COLUMN_TRIP_STARTLOCATION = "trip_startlocation";
+    private static final String COLUMN_TRIP_ENDLOCATION = "trip_endlocation";
+    private static final String COLUMN_TRIP_TYPE = "trip_type";
+    private static final String COLUMN_TRIP_USEREMAIL = "trip_userEmail";
+
+
     // create table sql query
     private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
             + COLUMN_USER_EMAIL + " TEXT" + " primary key," + COLUMN_USER_NAME + " TEXT,"
             + COLUMN_USER_BDAY + " TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ")";
+    private String CREATE_TRIP_TABLE = "CREATE TABLE " + TABLE_TRIP + "("
+            + COLUMN_TRIP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_TRIP_TITLE + " TEXT,"
+            + COLUMN_TRIP_STARTDATE + " TEXT," + COLUMN_TRIP_ENDDATE + " TEXT,"
+            + COLUMN_TRIP_STARTLOCATION + " TEXT," + COLUMN_TRIP_ENDLOCATION
+            + " TEXT," + COLUMN_TRIP_TYPE + " TEXT," + COLUMN_TRIP_USEREMAIL + " TEXT" + ")";
     // drop table sql query
     private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER;
+    private String DROP_TRIP_TABLE = "DROP TABLE IF EXISTS " + TABLE_TRIP;
     /**
      * Constructor
      *
@@ -40,11 +59,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(CREATE_TRIP_TABLE);
+
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //Drop User Table if exist
         db.execSQL(DROP_USER_TABLE);
+        db.execSQL(DROP_TRIP_TABLE);
         // Create tables again
         onCreate(db);
     }
@@ -65,6 +87,36 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else
             return true;
     }
+
+    /**
+     * This method is to fetch a user and return the user records
+     *
+     * @return user
+     */
+    public User getUser(String email) {
+        // array of columns to fetch
+        String[] columns = {
+                COLUMN_USER_EMAIL,
+                COLUMN_USER_NAME,
+                COLUMN_USER_BDAY,
+                COLUMN_USER_PASSWORD
+        };
+        SQLiteDatabase db = this.getReadableDatabase();
+        // query the user table
+        Cursor cursor = db.rawQuery("Select * from user where user_email = ?", new String[] {email});
+        User user = new User();
+        // Traversing through all rows and adding to list
+        if (cursor.moveToFirst()) {
+                user.setBirthday(cursor.getString(cursor.getColumnIndex(COLUMN_USER_BDAY)));
+                user.setName(cursor.getString(cursor.getColumnIndex(COLUMN_USER_NAME)));
+                user.setEmail(cursor.getString(cursor.getColumnIndex(COLUMN_USER_EMAIL)));
+                user.setPassword(cursor.getString(cursor.getColumnIndex(COLUMN_USER_PASSWORD)));
+        }
+        cursor.close();
+        db.close();
+        // return user
+        return user;
+    }
     /**
      * This method to check user exist or not
      *
@@ -84,7 +136,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return false;
     }
     /**
-     * This method to check user exist or not
+     * This method is to check user exist or not
      *
      * @param email
      * @param password
@@ -103,22 +155,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
         return false;
     }
-//    /**
-//     * This method to update user record
-//     *
-//     * @param user
-//     */
-//    public void updateUser(User user) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues values = new ContentValues();
-//        values.put(COLUMN_USER_NAME, user.getName());
-//        values.put(COLUMN_USER_EMAIL, user.getEmail());
-//        values.put(COLUMN_USER_PASSWORD, user.getPassword());
-//        // updating row
-//        db.update(TABLE_USER, values, COLUMN_USER_ID + " = ?",
-//                new String[]{String.valueOf(user.getId())});
-//        db.close();
-//    }
+    /**
+     * This method is to update user record
+     *
+     * @param user
+     */
+    public void updateUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USER_NAME, user.getName());
+        values.put(COLUMN_USER_EMAIL, user.getEmail());
+        values.put(COLUMN_USER_PASSWORD, user.getPassword());
+        // updating row
+        db.update(TABLE_USER, values, COLUMN_USER_EMAIL + " = ?",
+                new String[]{String.valueOf(user.getEmail())});
+        db.close();
+    }
 //    /**
 //     * This method is to delete user record
 //     *
@@ -132,4 +184,92 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 //        db.close();
 //    }
 
+    /** This method is to create a Trip record
+     *
+     * @param trip
+     * @param userEmail
+     * @return
+     */
+    public Boolean addTrip(Trip trip, String userEmail) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TRIP_TITLE, trip.getTripTitle());
+        values.put(COLUMN_TRIP_STARTDATE, trip.getStartDate());
+        values.put(COLUMN_TRIP_ENDDATE, trip.getEndDate());
+        values.put(COLUMN_TRIP_STARTLOCATION, trip.getStartLocation());
+        values.put(COLUMN_TRIP_ENDLOCATION, trip.getEndLocation());
+        values.put(COLUMN_TRIP_USEREMAIL, userEmail);
+        // Inserting Row
+        long result = db.insert(TABLE_TRIP, null, values);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public List<Trip> getTripsByUser(String email) {
+
+        List<Trip> trips = new ArrayList<Trip>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {
+                COLUMN_TRIP_ID,
+                COLUMN_TRIP_TITLE,
+                COLUMN_TRIP_STARTDATE,
+                COLUMN_TRIP_ENDDATE,
+                COLUMN_TRIP_STARTLOCATION,
+                COLUMN_TRIP_ENDLOCATION,
+                COLUMN_TRIP_USEREMAIL
+        };
+        Cursor cursor = db.rawQuery("Select trip_title, trip_startdate, trip_enddate, " +
+                "trip_startlocation, trip_endlocation from trip where user_email = ? order by trip_id"
+                ,new String[]{email});
+        if(cursor.moveToFirst()) {
+            do{
+                Trip trip = new Trip();
+                trip.setTripTitle(cursor.getString(cursor.getColumnIndex(COLUMN_TRIP_TITLE)));
+                trip.setStartDate(cursor.getString(cursor.getColumnIndex(COLUMN_TRIP_STARTDATE)));
+                trip.setEndDate(cursor.getString(cursor.getColumnIndex(COLUMN_TRIP_ENDDATE)));
+                trip.setStartLocation(cursor.getString(cursor.getColumnIndex(COLUMN_TRIP_STARTLOCATION)));
+                trip.setEndDate(cursor.getString(cursor.getColumnIndex(COLUMN_TRIP_ENDLOCATION)));
+                trips.add(trip);
+            }while(cursor.moveToNext());
+
+        }
+        cursor.close();
+        db.close();
+        return trips;
+    }
+
+    /**
+     * This method to update trip record
+     *
+     * @param trip
+     */
+    public void updateTrip(Trip trip) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TRIP_TITLE, trip.getTripTitle());
+        values.put(COLUMN_TRIP_STARTDATE, trip.getStartDate());
+        values.put(COLUMN_TRIP_ENDDATE, trip.getEndDate());
+        values.put(COLUMN_TRIP_STARTLOCATION, trip.getStartLocation());
+        values.put(COLUMN_TRIP_ENDLOCATION, trip.getEndLocation());
+        values.put(COLUMN_TRIP_TYPE, trip.getType());
+        // updating row
+        db.update(TABLE_TRIP, values, COLUMN_TRIP_ID + " = ?",
+                new String[]{String.valueOf(trip.getId())});
+        db.close();
+    }
+
+        /**
+     * This method is to delete trip record
+     *
+     * @param trip
+     */
+    public void deleteUser(Trip trip) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        // delete trip record by id
+        db.delete(TABLE_TRIP, COLUMN_TRIP_ID + " = ?",
+                new String[]{String.valueOf(trip.getId())});
+        db.close();
+    }
 }
