63663fe10704d4b8f89bd6158e1c654b9a8da25a
j3jabanes <70612391+j3jabanes@users.noreply.github.com>
2021-08-26 15:19:30 +0800

    Updated TripsActivity.java

--- TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/TripsActivity.java
+++ TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/TripsActivity.java
@@ -1,13 +1,21 @@
 package com.mobdeve.group17.triptripmobileapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -17,18 +25,19 @@ public class TripsActivity extends AppCompatActivity {
     private ArrayList<Trip> dataTrips;
     private RecyclerView.LayoutManager layoutManager;
     private TripAdapter adapter;
+    private FloatingActionButton fabAdd, fabEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trips);
-
         initDropdown();
         initRecyclerView();
+        Intent intent = getIntent();
+        String curr_user = intent.getStringExtra(MainActivity.KEY_CURR_USER);
     }
 
-    public void initRecyclerView(){
-
+    public void initRecyclerView() {
         this.rv_trips = findViewById(R.id.rv_trips);
         this.dataTrips = new DataHelper().getTrips();
 
@@ -38,8 +47,18 @@ public class TripsActivity extends AppCompatActivity {
         this.adapter = new TripAdapter(this.dataTrips);
         this.rv_trips.setAdapter(this.adapter);
 
-    }
+        this.fabAdd = findViewById(R.id.fab_add_trip);
+        this.fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TripsActivity.this, AddTripActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        Log.d("message", "main working");
+
+    }
     public void initDropdown(){
         Spinner dropdown = (Spinner) findViewById(R.id.dropdown_sort_date);
 
