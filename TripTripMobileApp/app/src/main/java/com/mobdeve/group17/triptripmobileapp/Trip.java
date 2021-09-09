4850185061c00ae711bf1e6054ccccc544fb0734
ely-ong <79676314+ely-ong@users.noreply.github.com>
2021-09-10 03:58:19 +0800

    Update Trip.java

--- TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/Trip.java
+++ TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/Trip.java
@@ -1,8 +1,9 @@
 package com.mobdeve.group17.triptripmobileapp;
 
 public class Trip {
-    private int tripPicId;
-    private String tripTitle, startDate, endDate, startLocation, endLocation, type, description;
+    private int tripPicId, id;
+    private String tripTitle, startDate, endDate, startLocation,
+            endLocation, type, userEmail, description;
 
     public Trip(int tripPicId, String title, String startDate, String endDate, String startLocation, String endLocation,
                 String type, String description) {
@@ -16,36 +17,85 @@ public class Trip {
         this.description = description;
     }
 
+    public Trip(){}
+
     public int getTripPicId() {
         return tripPicId;
+    }
+
+    public void setTripPicId(int tripPicId) {
+        this.tripPicId = tripPicId;
     }
 
     public String getTripTitle() {
         return tripTitle;
     }
 
+    public void setTripTitle(String tripTitle) {
+        this.tripTitle = tripTitle;
+    }
+
     public String getStartDate() {
         return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
     }
 
     public String getEndDate() {
         return endDate;
     }
 
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
     public String getStartLocation() {
         return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
     }
 
     public String getEndLocation() {
         return endLocation;
     }
 
+    public void setEndLocation(String endLocation) {
+        this.endLocation = endLocation;
+    }
+
     public String getTripType() {
         return type;
+    }
+
+    public void setTripType(String type) {
+        this.type = type;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
 }
