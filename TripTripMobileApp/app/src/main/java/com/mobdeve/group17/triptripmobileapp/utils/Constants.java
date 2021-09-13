da0cd427f1b46d301c42ae52606019090c761d4b
j3jabanes <70612391+j3jabanes@users.noreply.github.com>
2021-09-13 23:23:06 +0800

    Added files for SharedPreferences

---
+++ TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.mobdeve.group17.triptripmobileapp.utils;
+
+public class Constants {
+    public static String KEY_EMAIL = "email";
+    public static String KEY_PASSWORD = "password";
+    public static String KEY_NAME = "name";
+    public static String KEY_BDAY = "bday";
+}
