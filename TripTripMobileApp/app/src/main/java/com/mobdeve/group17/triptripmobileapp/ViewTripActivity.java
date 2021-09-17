dc8cca714cb423bee2a87be13efecdf170f8972c
ely-ong <79676314+ely-ong@users.noreply.github.com>
2021-09-17 11:00:26 +0800

    Update ViewTripActivity.java

--- TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/ViewTripActivity.java
+++ TripTripMobileApp/app/src/main/java/com/mobdeve/group17/triptripmobileapp/ViewTripActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,9 +49,13 @@ public class ViewTripActivity extends AppCompatActivity {
         this.tv_endLocation.setText(trip.getEndLocation());
         this.tv_descrip.setText(trip.getDescription());
 
+        //if trip pic exists
         if(trip.getTripPicId() != null) {
             this.iv_view_trip_pic.setVisibility(View.VISIBLE);
             this.iv_view_trip_pic.setImageBitmap(ImageHelper.toImage(trip.getTripPicId()));
+        }
+        else{
+            this.iv_view_trip_pic.setVisibility(View.GONE);
         }
 
         btnEditTrip.setOnClickListener(new View.OnClickListener() {
